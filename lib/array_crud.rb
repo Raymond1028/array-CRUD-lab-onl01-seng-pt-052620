080d48a67c97c1a916f7525bfc6d2a027873eb6d
Raymond Sims <lancelot53.rs@gmail.com>
2020-06-07 18:51:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,7 +34,8 @@ def remove_element_from_start_of_array(array)
   end
 
 def retrieve_element_from_index(array, index_number)
-  array_1 = ["paul,"ringo","john"]
+  array_1 = ["paul","ringo","john"]
+  array_1[index_number]
 end
 
 def retrieve_first_element_from_array(array)
