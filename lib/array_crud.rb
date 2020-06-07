2fba2ebce6f91ccfa260ca13dff675d7b1a48e24
Raymond Sims <lancelot53.rs@gmail.com>
2020-06-07 18:41:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array_1 = ["bananas","arrays"]
+  array_1 = ["bananas","arrays!"]
   array_1.pop
 end
   def retrieve_last_element_from_array
